be52439902e32610dc5bb0149bdda2ae0f21b662
Cândea Marian <candea.dev@gmail.com>
2023-12-20 21:27:32 +0200

    - created a brute working version of the question flow

---
+++ scripts/main.js
@@ -0,0 +1,122 @@
+import { scienceQuestions } from "questions.js";
+
+var currentQuestionIndex = 0;
+var score = 0;
+var userAnswers = [];
+
+/* Randomize array in-place using Durstenfeld shuffle algorithm */
+function shuffleArray(array) {
+  for (var i = array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+}
+
+// Function to render a question
+function renderQuestion(question) {
+  const questionElement = document.getElementById("question");
+  const choicesContainer = document.querySelector(".btn-group-vertical");
+
+  // Check if the element with ID "question" exists
+  if (!questionElement) {
+    console.error('Element with ID "question" not found');
+    return;
+  }
+
+  // Clear previous content
+  questionElement.textContent = question.question;
+  choicesContainer.innerHTML = "";
+
+  // Populate choices
+  question.choices.forEach((choice, index) => {
+    const input = document.createElement("input");
+    input.type = "radio";
+    input.className = "btn-check";
+    input.name = "btnradio";
+    input.id = `btnradio${index + 1}`;
+    input.autocomplete = "off";
+
+    const label = document.createElement("label");
+    label.className = "btn btn-outline-primary border-custom";
+    label.htmlFor = `btnradio${index + 1}`;
+    label.textContent = choice;
+
+    choicesContainer.appendChild(input);
+    choicesContainer.appendChild(label);
+
+    // Add event listener for radio button click
+    input.addEventListener("click", function () {
+      submitAnswer(choice);
+    });
+  });
+}
+
+function submitAnswer(selectedAnswer) {
+  const correctAnswer = scienceQuestions[currentQuestionIndex].correctAnswer;
+
+  // Check if the selected answer is correct
+  if (selectedAnswer === correctAnswer) {
+    score++;
+  }
+
+  if (selectedAnswer) {
+    userAnswers.push({
+      question: scienceQuestions[currentQuestionIndex].question,
+      correctAnswer: correctAnswer,
+      userAnswer: selectedAnswer,
+    });
+  }
+
+  // Move to the next question or finish the quiz
+  if (currentQuestionIndex < scienceQuestions.length - 1) {
+    currentQuestionIndex++;
+    renderQuestion(scienceQuestions[currentQuestionIndex]);
+  } else {
+    // Remove the 'main' element
+    const questionBlock = document.getElementById("question");
+    const choicesBlock = document.querySelector(".btn-group-vertical");
+    questionBlock.parentNode.removeChild(questionBlock);
+    choicesBlock.parentNode.removeChild(choicesBlock);
+
+    // Display final score
+    alert(`Quiz complete! Your score: ${score}/${scienceQuestions.length}`);
+    displayResultTable();
+  }
+}
+
+function displayResultTable() {
+  // Create a table element
+  const resultTable = document.createElement("table");
+  resultTable.border = "1";
+
+  // Create table header
+  const headerRow = resultTable.createTHead().insertRow();
+  ["Question", "Correct Answer", "User Answer"].forEach((headerText) => {
+    const th = document.createElement("th");
+    th.textContent = headerText;
+    headerRow.appendChild(th);
+  });
+
+  // Create table body
+  const resultTableBody = resultTable.createTBody();
+  userAnswers.forEach((answer) => {
+    const row = resultTableBody.insertRow();
+    ["question", "correctAnswer", "userAnswer"].forEach((prop) => {
+      const cell = row.insertCell();
+      cell.textContent = answer[prop];
+    });
+  });
+
+  // Apply styles to center the table
+  resultTable.style.margin = "auto";
+  resultTable.style.textAlign = "center";
+
+  // Append the table to the document body (or another target element)
+  const questionContainer = document.getElementById("question-container");
+  questionContainer.appendChild(resultTable);
+}
+
+shuffleArray(scienceQuestions);
+renderQuestion(scienceQuestions[0]);
---
+++ scripts/questions.js
@@ -0,0 +1,108 @@
+const scienceQuestions = [
+  {
+    id: 1,
+    question: "What is the theoretical maximum density of a black hole?",
+    choices: ["1 g/cm³", "1,000 g/cm³", "Infinite", "100 g/cm³"],
+    correctAnswer: "Infinite",
+  },
+  {
+    id: 2,
+    question:
+      "In organic chemistry, what is the term for a reaction where a molecule gains electrons?",
+    choices: ["Oxidation", "Reduction", "Isomerization", "Substitution"],
+    correctAnswer: "Reduction",
+  },
+  {
+    id: 3,
+    question:
+      "What is the minimum number of dimensions required for string theory to be mathematically consistent?",
+    choices: ["3", "4", "10", "11"],
+    correctAnswer: "10",
+  },
+  {
+    id: 4,
+    question: "Which element has the highest melting point?",
+    choices: ["Tungsten", "Rhenium", "Osmium", "Platinum"],
+    correctAnswer: "Rhenium",
+  },
+  {
+    id: 5,
+    question:
+      "In genetics, what is the term for a sudden, marked variation in an individual's DNA sequence?",
+    choices: ["Mutation", "Polymorphism", "Allele", "Genotype"],
+    correctAnswer: "Mutation",
+  },
+  {
+    id: 6,
+    question: "What is the most abundant gas in the Earth's atmosphere?",
+    choices: ["Oxygen", "Nitrogen", "Carbon Dioxide", "Argon"],
+    correctAnswer: "Nitrogen",
+  },
+  {
+    id: 7,
+    question: "Which subatomic particle is responsible for chemical bonding?",
+    choices: ["Proton", "Neutron", "Electron", "Quark"],
+    correctAnswer: "Electron",
+  },
+  {
+    id: 8,
+    question: "What is the largest planet in our solar system?",
+    choices: ["Earth", "Jupiter", "Saturn", "Neptune"],
+    correctAnswer: "Jupiter",
+  },
+  {
+    id: 9,
+    question: "In biochemistry, what is the 'central dogma'?",
+    choices: [
+      "DNA replication",
+      "RNA transcription",
+      "Protein translation",
+      "All of the above",
+    ],
+    correctAnswer: "All of the above",
+  },
+  {
+    id: 10,
+    question: "What is the speed of light in a vacuum (approximately)?",
+    choices: ["300,000 km/s", "150,000 km/s", "600,000 km/s", "1,000,000 km/s"],
+    correctAnswer: "300,000 km/s",
+  },
+  {
+    id: 11,
+    question:
+      "What is the process by which plants make their own food using sunlight?",
+    choices: ["Photosynthesis", "Respiration", "Fermentation", "Transpiration"],
+    correctAnswer: "Photosynthesis",
+  },
+  {
+    id: 12,
+    question: "Which element is essential for all organic compounds?",
+    choices: ["Carbon", "Oxygen", "Hydrogen", "Nitrogen"],
+    correctAnswer: "Carbon",
+  },
+  {
+    id: 13,
+    question: "What is the largest organ in the human body?",
+    choices: ["Heart", "Brain", "Skin", "Liver"],
+    correctAnswer: "Skin",
+  },
+  {
+    id: 14,
+    question: "What is the speed of light in a vacuum?",
+    choices: [
+      "299,792 kilometers per second",
+      "150,000 kilometers per second",
+      "500,000 kilometers per second",
+      "200,000 kilometers per second",
+    ],
+    correctAnswer: "299,792 kilometers per second",
+  },
+  {
+    id: 15,
+    question: "Which gas is responsible for the greenhouse effect on Earth?",
+    choices: ["Carbon Dioxide", "Oxygen", "Nitrogen", "Methane"],
+    correctAnswer: "Carbon Dioxide",
+  },
+];
+
+export { scienceQuestions };
